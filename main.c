279248bd00c5e91f485e4194143f082df989f215
chenjr15 <995180602@qq.com>
2016-11-29 13:37:06 +0800

    contribute to main()
    Signed-off-by: chenjr15 <995180602@qq.com>

--- main.c
+++ main.c
@@ -12,7 +12,23 @@ void delay(uint delays)
 
 void main()
 {
+	uint i=0,j=0,k=0;
+	while(1){	
+	for (;i<100;i++){
+	led0=0;
+	delay(10*i);
+	led0=1;
+	delay(100);
+	}
+	for (;i>0;i--){
+	led0=0;
+	delay(10*i);
+	led0=1;
+	delay(100);
+	}
 	
+	
+	}
 	
 	
 }
